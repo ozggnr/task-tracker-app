d8ce80a5c2710dbdf8fc4c0cd161533c5046c233
ozge <ozgecaylioglu@gmail.com>
2022-11-17 16:50:56 -0500

    added progress bar component

--- task-app/src/components/progressBar/ProgressBar.tsx
+++ task-app/src/components/progressBar/ProgressBar.tsx
@@ -1,57 +1,66 @@
-import { useState, useEffect, useRef } from "react";
-import moment from "moment";
+import { useState, useEffect, useRef } from 'react';
+import moment from 'moment';
+import './styles.css';
 
 export const ProgressBar = () => {
     const [date, setDate] = useState(new Date());
-    const intervalRef = useRef<number>();
-    const endTime = "16:46";
+    const intervalRef = useRef();
+    const endTime = '23:00';
 
-    useEffect(() => {
-        const timerId = setInterval(() => setDate(new Date()), 1000);
-        intervalRef.current = timerId;
-        return function cleanup() {
-        clearInterval(intervalRef.current);
-        };
-    }, []);
-
+    function refreshClock() {
+        console.log('---------------', intervalRef);
+        setDate(new Date());
+    }
     function convertStringToTime(date: Date, time: string) {
-        const dateString = moment(date).format("L");
-        return moment(`${dateString} ${time}`).format("LTS");
+        const dateString = moment(date).format('L');
+        return moment(`${dateString} ${time}`).format('LTS');
     }
     const convertedEndTime = convertStringToTime(date, endTime);
-    const nowTime = moment(date).format("LTS");
+    const nowTime = moment(date).format('LTS');
 
-    const startTime = moment(nowTime, "HH:mm:ss a");
-    const endedTime = moment(convertedEndTime, "HH:mm:ss a");
+    const startTime = moment(nowTime, 'HH:mm:ss a');
+    const endedTime = moment(convertedEndTime, 'HH:mm:ss a');
     const dif = moment.duration(endedTime.diff(startTime));
-
-    console.log(`${dif.hours()} hours, ${dif.minutes()} mins, ${dif.seconds()} secs`);
-
-    const totalSecs = Math.floor(
-        dif.hours() * 3600 + dif.minutes() * 60 + dif.seconds()
+    console.log(
+        `${dif.hours()} hours, ${dif.minutes()} mins, ${dif.seconds()} secs`
     );
 
+    const totalSecs = dif.hours() * 3600 + dif.minutes() * 60 + dif.seconds();
+    const completed = 360 - totalSecs / 360;
 
-    let completed = 100 - totalSecs / 100;
-
-    if (completed === 100) {
-        clearInterval(intervalRef.current);
-    }
+    // useEffect(() => {
+    //   const timerId = setInterval(refreshClock, 1000);
+    //   intervalRef.current = timerId;
+    //   return function cleanup() {
+    //     clearInterval(intervalRef.current);
+    //   };
+    // }, []);
 
     const fillerStyles: React.CSSProperties = {
-        height: "100%",
-        width: `${completed}%`,
-        backgroundColor: "red",
-        borderRadius: "inherit",
-        textAlign: "right"
+        position: 'relative',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '250px',
+        width: '250px',
+        borderRadius: '50%',
+        border: '2px solid black',
+        zIndex: '0',
+        // background: `conic-gradient(red ${Math.floor(completed)}deg, #fff 0deg)`
     };
-    return <div>
-        <div>{date.toLocaleTimeString()}</div>
+
+    if (completed === 360) {
+        clearInterval(intervalRef.current);
+    }
+    return (
         <div className="progressContainer">
             <div style={fillerStyles}>
-                <span>{`${completed}%`}</span>
-                {completed === 100 && <div>Completed!</div>}
+                {/* <span className="sth">{`${Math.floor(completed)}%`}</span> */}
+                <div className="progress-value">
+                    {date.toLocaleTimeString()}
+                </div>
+                {completed === 360 && <div>Completed!</div>}
             </div>
         </div>
-    </div>
-}
+    );
+};
